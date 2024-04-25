750a1fa84e139a8e70a70f6ba4d3d186e4c07d68
Jefferson Laurindo da Silva <107891837+Jefflds@users.noreply.github.com>
2024-04-25 02:50:46 +0000

    refactor: refactor filter component

---
+++ src/ui/components/Filter/Filter.tsx
@@ -0,0 +1,11 @@
+const Filter = () => {
+    return (
+        <select title="filter">
+            <option value="Todos">Todos</option>
+            <option value="Concluídos">Concluídos</option>
+            <option value="Pendentes">Pendentes</option>
+        </select>
+    )
+}
+
+export default Filter;
--- src/ui/partials/AddButtonAndFilter/AddButtonAndFilter.tsx
+++ src/ui/partials/AddButtonAndFilter/AddButtonAndFilter.tsx
@@ -1,15 +1,12 @@
 import { Button } from "react-bootstrap";
 import { AddButtonAndFilterContainer } from "../../styles/AddButtonAndFilter/AddButtonAndFilter.style";
+import Filter from "../../components/Filter/Filter";
 
 const AddButtonAndFilter = () => {
     return (
         <AddButtonAndFilterContainer className="w-100 d-flex align-items-center justify-content-between">
             <Button>Adicionar Tarefa</Button>
-            <select title="filter">
-                <option value="Todos">Todos</option>
-                <option value="Concluídos">Concluídos</option>
-                <option value="Pendentes">Pendentes</option>
-            </select>
+            <Filter />
         </AddButtonAndFilterContainer>
     )
 }
