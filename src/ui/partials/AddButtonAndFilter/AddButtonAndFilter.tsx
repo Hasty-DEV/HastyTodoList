a4dba155bb3b2cd4169a5158c453f26aa06f6972
Jefferson Laurindo da Silva <107891837+Jefflds@users.noreply.github.com>
2024-04-26 23:12:37 +0000

    feat: Add Create Item Logic

--- src/ui/partials/AddButtonAndFilter/AddButtonAndFilter.tsx
+++ src/ui/partials/AddButtonAndFilter/AddButtonAndFilter.tsx
@@ -1,13 +1,59 @@
 import { Button } from "react-bootstrap";
 import { AddButtonAndFilterContainer } from "../../styles/AddButtonAndFilter/AddButtonAndFilter.style";
 import Filter from "../../components/Filter/Filter";
+import { FormEvent, RefObject, useRef, useState } from "react";
+import { FaPlus } from "react-icons/fa";
+import { ItemType } from "../../../data/@types/Item/Item.type";
 
 const AddButtonAndFilter = () => {
+    const [modal, setModal] = useState<boolean>();
+    const titleRef: RefObject<HTMLInputElement> = useRef(null);
+
+    const HandleOpenModal = () => {
+        setModal(true)
+    }
+
+    const HandleOnSubmit = (e: FormEvent) => {
+        e.preventDefault();
+        const title = titleRef.current?.value;
+        if (!title) {
+            alert("Por favor, insira um título para a tarefa.");
+            return;
+        }
+        const date = Date.now();
+        const checked = false;
+        const newItem: ItemType = {
+            ItemTitle: title,
+            ItemData: date,
+            isChecked: checked
+        };
+        let listItems: ItemType[] = JSON.parse(localStorage.getItem("ListItems") || "[]");
+        listItems.push(newItem);
+        localStorage.setItem("ListItems", JSON.stringify(listItems));
+        titleRef.current!.value = "";
+        setModal(false);
+    };
+
+
     return (
-        <AddButtonAndFilterContainer className="w-100 d-flex align-items-center justify-content-between">
-            <Button>Adicionar Tarefa</Button>
-            <Filter />
-        </AddButtonAndFilterContainer>
+        <>
+            <AddButtonAndFilterContainer className="w-100 d-flex align-items-center justify-content-between">
+                {!modal ? (
+                    <Button onClick={HandleOpenModal} >Adicionar Tarefa</Button>
+                ) :
+                    (
+                        <Button disabled>Adicionar Tarefa</Button>
+                    )
+                }
+                <Filter />
+            </AddButtonAndFilterContainer>
+            {modal && (
+                <form className="w-100 pt-2 pb-2 d-flex form-group" onSubmit={(e) => HandleOnSubmit(e)}>
+                    <input ref={titleRef} type="text" className="w-100 form-control" />
+                    <Button type="submit" className="mx-2"><FaPlus /></Button>
+                </form>
+            )}
+        </>
     )
 }
 
--- src/ui/partials/List/List.tsx
+++ src/ui/partials/List/List.tsx
@@ -1,17 +1,24 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import { ItemType } from "../../../data/@types/Item/Item.type";
-import { ListContainer } from "../../styles/List/List.style"
+import { ListContainer } from "../../styles/List/List.style";
 import Item from "../../components/Item/Item";
 
 const List = () => {
-    const [items, setItems] = useState<ItemType[]>([
-        { ItemTitle: "Criar o primeiro projeto React", ItemData: Date.now() },
-    ]);
+    const [items, setItems] = useState<ItemType[]>([]);
+
+    useEffect(() => {
+        const storedItems = localStorage.getItem("ListItems");
+        if (storedItems) {
+            const parsedItems: ItemType[] = JSON.parse(storedItems);
+            setItems(parsedItems);
+        }
+    }, []);
 
     const handleItemCheck = (index: number, isChecked: boolean) => {
         const updatedItems = [...items];
         updatedItems[index].isChecked = isChecked;
         setItems(updatedItems);
+        localStorage.setItem("ListItems", JSON.stringify(updatedItems));
     };
 
     return (
@@ -26,7 +33,7 @@ const List = () => {
                 />
             ))}
         </ListContainer>
-    )
-}
+    );
+};
 
-export default List
+export default List;
